bcf5ffa8929aa15948390704e859b3dd14f869c8
Christofer Engel <christofer.engel@kambi.com>
2018-03-02 17:13:31 +0100

    Added new components to index.js

--- src/index.js
+++ src/index.js
@@ -6,5 +6,11 @@ import Button from './Button.js';
 import Elem from './Elem.js';
 import RootElem from './RootElem.js';
 import Txt from './Txt.js';
+import Checkbox from './Checkbox.js';
+import Radio from './Radio.js';
+import Textarea from './Textarea.js';
+import Select from './Select.js';
+import Transition from './Transition.js';
+import Input from './Input.js';
 
-export { Button, Elem, RootElem, Txt };
+export { Button, Elem, RootElem, Txt, Checkbox, Radio, Textarea, Select, Transition, Input };
