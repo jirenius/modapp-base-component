7c78a081e51639a53e3e3e5f137c74c528eb9190
Samuel Jirénius <samuel@jirenius.com>
2018-06-24 15:58:49 +0200

    Added generateName function to export. Fixed bug in rollup.config.js.

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import Checkbox from './Checkbox.js';
 import Elem from './Elem.js';
 import Html from './Html.js';
 import Input from './Input.js';
-import Radio from './Radio.js';
+import Radio, { generateName } from './Radio.js';
 import RootElem from './RootElem.js';
 import Select from './Select.js';
 import Textarea from './Textarea.js';
 import Transition from './Transition.js';
 import Txt from './Txt.js';
 
-export { Button, Checkbox, Elem, Html, Input, Radio, RootElem, Select, Textarea, Transition, Txt };
+export { Button, Checkbox, Elem, Html, Input, Radio, RootElem, Select, Textarea, Transition, Txt, generateName };
